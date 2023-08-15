51d6d0be6b15bce6e42da3ecd2082dfb1ffa3e59
Rory Kelly <rory@ucar.edu>
2023-08-15 12:53:34 -0600

    More incremental progress

--- svg_pp.jl
+++ svg_pp.jl
@@ -3,9 +3,10 @@ using Printf
 
 # post-process the SVG file to move the node labels
 function svg_pp()
-   sdvec=Dict{String, Vector{Float64}}() # dictionary to hold node --> displacement vector mappings
-   active_sdvec=Vector{Float64}()        # displacement vector for the current node
-   move_text=false                       # flag to indicate if we are moving text on the next line
+   sdvec=Dict{String, Vector{Float64}}()         # dictionary to hold node --> displacement vector mappings
+   active_sdvec=Vector{Float64}()                # displacement vector for the current node
+   sg_new_coords=Dict{String, Vector{Float64}}() # dictionary to hold switch group --> coordinate mappings
+   move_text=false                               # flag to indicate if we are moving text on the next line
 	# read file by lines into an array
 	ifile=open("derecho.desc.svg")
 	lines=readlines(ifile)
@@ -40,16 +41,20 @@ function svg_pp()
    node_rexp=r"<title>(de.{1}[0-9]+)</title>"
    # used to find switch groups
    sg_rexp=r"<title>(x[0-9]{4})</title>"
+   # used to find both switch groups on an edge
+   sg2sg_names_rexp=r"<title>(x[0-9]{4}).+(x[0-9]{4})</title>"
    # used to find node boxes that need to be moved
    node_box_rexp=r"<polygon fill=\"white\"\s+stroke=\"blue\"\s+points="
    # used to find switch group ellipses that need to be moved
    sg_ellipse_rexp=r"<ellipse fill=\"white\" stroke=\"brown\"\s+cx="
+   # used to extract x,y center of an ellipse
+   ellipse_ctr_rexp=r"cx=\"(-?[0-9]+\.?[0-9]*)\"\s+cy=\"(-?[0-9]+\.?[0-9]*)\""
    
    #-- loop through file and calculate displacement vectors for each node label
    for (i,l) in enumerate(lines)
       # try to match various edge and node types 
       n2s_edge_m=match(node2switch_rexp,l)
-      sg2sg_edge_m=match(sg_rexp,l)
+      sg2sg_edge_m=match(sg2sg_rexp,l)
       lsg2sg_edge_m=match(lsg2sg_rexp,l)
       node_m=match(node_rexp,l)
       node_box_m=match(node_box_rexp,l)
@@ -64,6 +69,22 @@ function svg_pp()
          dvec=p1-p2
          sfac=37/sqrt(dvec[1]^2+dvec[2]^2)
          sdvec[node]=sfac*dvec
+         write(ofile,l*"\n")
+      elseif(sg2sg_edge_m!=nothing)
+         #println("Found an sg all-to-all edge: $l")
+         # find line between endpoints, and normalized perpendicular vector
+         #p1=str_to_vec(sg2sg_edge_m.captures[1])
+         #p2=str_to_vec(sg2sg_edge_m.captures[4])
+         #line=p1-p2
+         #perp=[line[2],-line[1]]
+         #perp=(500/sqrt(perp[1]^2+perp[2]^2))*perp
+         ## translate points in perpendicular direction, and output a new edge as a line
+         ## instead of a bezier curve
+         #new_p1 = p1-perp
+         #new_p2 = p2-perp
+         #new_edge = "<path fill=\"none\" stroke=\"red\" stroke-width=\"2\" d=\"M$(new_p1[1]),$(new_p1[2])L$(new_p2[1]),$(new_p2[2])\"/>"
+         #println("New edge: $new_edge")
+         ## Do nothing on first pass
          write(ofile,l*"\n")
       elseif(lsg2sg_edge_m!=nothing)
          println("Logical SG to SG edge: $(lsg2sg_edge_m.captures[1]), $(lsg2sg_edge_m.captures[2]) $(lsg2sg_edge_m.captures[3]) $(lsg2sg_edge_m.captures[4])")
@@ -91,7 +112,17 @@ function svg_pp()
          write(ofile,new_box(l,active_sdvec)*"\n")
          move_text=true
       elseif(sg_ellipse_m!=nothing) # move the ellipse to the new location
-         write(ofile,new_ellipse(l,active_sdvec)*"\n")
+         # get switch group name
+         sgm=match(sg_rexp,lines[i-1])
+         sg=sgm.captures[1]
+         ne=new_ellipse(l,active_sdvec) 
+         write(ofile,ne*"\n")
+         # keep coordinates of new elipse center for 2nd pass
+         println("NE: $ne")
+         el_ctr=match(ellipse_ctr_rexp,ne)
+         new_coords_str="$(el_ctr.captures[1]),$(el_ctr.captures[2])"
+         sg_new_coords[sg]=str_to_vec(new_coords_str)
+         println("switchGroup $sg, coords: $(sg_new_coords[sg])")
          move_text=true
       else
          if(move_text)
@@ -101,6 +132,28 @@ function svg_pp()
          else
             write(ofile,l*"\n")
          end
+      end
+   end
+   close(ofile)
+
+   # now make a 2nd pass
+   ifile=open("docs/derecho.desc.pp.svg")
+   lines=readlines(ifile)
+   close(ifile)
+   ofile=open("docs/derecho.desc.pp2.svg","w")
+   for (i,l) in enumerate(lines)
+      sg2sg_edge_m=match(sg2sg_rexp,l)
+      if(sg2sg_edge_m!=nothing)
+         # get the two switchgroups being connected
+         sg_names=match(sg2sg_names_rexp,lines[i-1])
+         # get coordinates of the switch groups
+         sg1_coords=sg_new_coords[sg_names.captures[1]]
+         sg2_coords=sg_new_coords[sg_names.captures[2]]
+         # line between new switchgroups
+         new_edge="<path fill=\"none\" stroke=\"red\" stroke-width=\"2\" d=\"M$(sg1_coords[1]),$(sg1_coords[2])L$(sg2_coords[1]),$(sg2_coords[2])\"/>"
+         write(ofile,new_edge*"\n")      
+      else
+         write(ofile,l*"\n")
       end
    end
    close(ofile)
